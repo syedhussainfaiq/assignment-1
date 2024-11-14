94afa76cbcbc41ed9747e541d3cf40b047239f2a
SYED FAIQ HUSSAIN <159991416+syedhussainfaiq@users.noreply.github.com>
2024-11-14 19:56:23 +0500

    Add files via upload
    my first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+        <div className="flex min-h-screen items-center justify-center bg-gray-300">
+            
+                <h1 className="text-6xl font-bold text-blue-600">Hello World</h1>
+
+        
+            
+            
+        </div>
+    )
+};
